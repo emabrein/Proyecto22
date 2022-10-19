f564b0eaa12c6c35d15fc82dcbf2653b0b8b047d
emabrein <110540116+emabrein@users.noreply.github.com>
2022-10-19 09:34:25 -0300

    recupere todo

---
+++ Assets/Home/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void showHistoria()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void hideHistoria()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void playGame1()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void exitGame1()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+    public void tryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+}
