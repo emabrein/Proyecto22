3d9b6f45ab5dcfce1016669c4ee1e9ea431f8cf2
tomi.moha <47349429@est.ort.edu.ar>
2022-11-02 12:07:06 -0300

    a tomi le gusta laburar, y elegimos musica fornite

---
+++ Assets/Puerta2OnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta2OnTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject Puerta2;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Puerta2.transform.position += new Vector3(0, -2.36f, 0);
+    }
+
+}
