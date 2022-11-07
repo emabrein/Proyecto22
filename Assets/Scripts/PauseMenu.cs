8997bd3068f2096361ead59cc90a2f9b9e048bd0
emabrein <110540116+emabrein@users.noreply.github.com>
2022-11-07 17:17:09 -0300

    ingles luunes

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour 
 {
+
   public static bool GameIsPaused = false;
   public GameObject PauseMenuUI;
   
