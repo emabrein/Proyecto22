8eb182485bb45b1ab1d4bbb3b842aa111d876d28
emabrein <110540116+emabrein@users.noreply.github.com>
2022-11-23 13:25:40 -0300

    dsp de presentar

--- Assets/Scripts/PataformaSticky.cs
+++ Assets/Scripts/PataformaSticky.cs
@@ -20,4 +20,19 @@ public class PataformaSticky : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D2(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player2")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D2(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player2")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
 }
---
+++ Assets/Scripts/Sticky2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sticky2 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player2")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player2")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
